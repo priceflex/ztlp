ac552ca5714992301569522331c541a7080726c0
Steven Price <steve@techrockstars.com>
2026-04-14 21:53:30 +0000

    gateway: parse ZTLP_NS_SERVER env var into Application config
    The ns_client.ex identity lookups use Config.get(:ns_server_host) which
    reads from Application.get_env. This was defaulting to 127.0.0.1 because
    runtime.exs didn't parse the ZTLP_NS_SERVER env var. All handshakes
    stalled on NS lookup timeout against localhost.

--- gateway/config/runtime.exs
+++ gateway/config/runtime.exs
@@ -14,8 +14,28 @@ import Config
 config :logger, level: String.to_atom(System.get_env("ZTLP_LOG_LEVEL", "info"))
 
 if config_env() == :prod do
+  # Parse ZTLP_NS_SERVER="host:port" into ns_server_host / ns_server_port
+  ns_config =
+    case System.get_env("ZTLP_NS_SERVER") do
+      nil -> []
+      "" -> []
+      ns_str ->
+        case String.split(ns_str, ":") do
+          [host, port_str] ->
+            case :inet.parse_address(String.to_charlist(host)) do
+              {:ok, ip_tuple} ->
+                [ns_server_host: ip_tuple,
+                 ns_server_port: String.to_integer(port_str)]
+              _ ->
+                []
+            end
+          _ -> []
+        end
+    end
+
   config :ztlp_gateway,
-    port: String.to_integer(System.get_env("ZTLP_GATEWAY_PORT", "23097")),
-    session_timeout_ms: String.to_integer(System.get_env("ZTLP_GATEWAY_SESSION_TIMEOUT_MS", "300000")),
-    max_sessions: String.to_integer(System.get_env("ZTLP_GATEWAY_MAX_SESSIONS", "10000"))
+    [{:port, String.to_integer(System.get_env("ZTLP_GATEWAY_PORT", "23097"))},
+     {:session_timeout_ms, String.to_integer(System.get_env("ZTLP_GATEWAY_SESSION_TIMEOUT_MS", "300000"))},
+     {:max_sessions, String.to_integer(System.get_env("ZTLP_GATEWAY_MAX_SESSIONS", "10000"))}
+     | ns_config]
 end
